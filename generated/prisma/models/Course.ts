d5ea69f8fa9948e2a84a96707ec7cf92bd0b9ccb
Dakrilova <dakrilova06@gmail.com>
2026-06-14 14:30:31 +0300

    fix prisma postgres client

--- generated/prisma/models/Course.ts
+++ generated/prisma/models/Course.ts
@@ -203,7 +203,7 @@ export type CourseGroupByOutputType = {
   _max: CourseMaxAggregateOutputType | null
 }
 
-type GetCourseGroupByPayload<T extends CourseGroupByArgs> = Prisma.PrismaPromise<
+export type GetCourseGroupByPayload<T extends CourseGroupByArgs> = Prisma.PrismaPromise<
   Array<
     Prisma.PickEnumerable<CourseGroupByOutputType, T['by']> &
       {
@@ -1319,6 +1319,11 @@ export type CourseFindManyArgs<ExtArgs extends runtime.Types.Extensions.Internal
    * Skip the first `n` Courses.
    */
   skip?: number
+  /**
+   * {@link https://www.prisma.io/docs/concepts/components/prisma-client/distinct Distinct Docs}
+   * 
+   * Filter by unique combinations of Courses.
+   */
   distinct?: Prisma.CourseScalarFieldEnum | Prisma.CourseScalarFieldEnum[]
 }
 
--- generated/prisma/models/GroupMember.ts
+++ generated/prisma/models/GroupMember.ts
@@ -186,7 +186,7 @@ export type GroupMemberGroupByOutputType = {
   _max: GroupMemberMaxAggregateOutputType | null
 }
 
-type GetGroupMemberGroupByPayload<T extends GroupMemberGroupByArgs> = Prisma.PrismaPromise<
+export type GetGroupMemberGroupByPayload<T extends GroupMemberGroupByArgs> = Prisma.PrismaPromise<
   Array<
     Prisma.PickEnumerable<GroupMemberGroupByOutputType, T['by']> &
       {
@@ -1216,6 +1216,11 @@ export type GroupMemberFindManyArgs<ExtArgs extends runtime.Types.Extensions.Int
    * Skip the first `n` GroupMembers.
    */
   skip?: number
+  /**
+   * {@link https://www.prisma.io/docs/concepts/components/prisma-client/distinct Distinct Docs}
+   * 
+   * Filter by unique combinations of GroupMembers.
+   */
   distinct?: Prisma.GroupMemberScalarFieldEnum | Prisma.GroupMemberScalarFieldEnum[]
 }
 
